34e320329dc1e4fe3df6c9b30fc4fb30813c0d20
Lukáš Holec <biker.lukyn@gmail.com>
2025-10-09 08:02:49 +0200

    Update

---
+++ Codes.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main () {
+     // nastav strany a preference
+     int pocet_stran = 8;
+     printf("Pocet stran: %d\n", pocet_stran);
+
+    return 0;
+}
