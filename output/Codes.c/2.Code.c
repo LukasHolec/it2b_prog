ef6873799543110e00d729e6d79245b401f472cb
Lukáš Holec <biker.lukyn@gmail.com>
2025-10-09 09:03:20 +0200

    new

--- output/Codes.c/2.Code.c
+++ output/Codes.c/2.Code.c
@@ -34,21 +34,33 @@
         char odpoved2;
         char kostky;
         int pocet;
+        int soucet;
        
-        
+        // generovani 1. hodu kostky
         srand(time(NULL));
         int cislo = rand() % 6 + 1;
 
+        // pokracovani
         printf("Hodil jste cislo: %d. \n Chcete pokracovat? [Y/n]?", cislo);
         scanf("%c", &odpoved);
         if (odpoved == Y) {
             printf("Chcete si zvolit pocet kostek? [K] Nebo pocet stran kostky? [S]?");
+
             if (odpoved2 = K) {
                 printf("S kolika kostky chcete hrat?");
                 scnaf("%d", &pocet);
-                
+                for (int i = 0; i < pocet; i++) {
+                    srand(time(NULL));
+                int vicekostek = rand() % 6 + 1;
+                soucet = soucet + vicekostek;
+                }
+                printf("Soucet hodu %d kostek je: %d", pocet, soucet);
+            }
+            else if (odpoved2 = S) {
 
             }
+            {
+
              
             else()
         } else {
