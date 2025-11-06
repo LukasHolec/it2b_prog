3a1abe89eb03bd920b80fb164c213c4d38ac881a
Lukáš Holec <biker.lukyn@gmail.com>
2025-11-06 09:00:00 +0100

    Update

--- output/Codes.c/4 Code.c
+++ output/Codes.c/4 Code.c
@@ -225,8 +225,8 @@ int main() {
                 scanf("%d", &arr[i]);
             }
 
-        for (int j = 0l j < 10; j++) {
-            
+        for (int j = 0; j < 10; j++) {
+
         }
 
 
--- output/Codes.c/5 Code.c
+++ output/Codes.c/5 Code.c
@@ -1,10 +1,49 @@
+/* 
+
+STRINGY
+-------------------------
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
     int main () {
+
+        char kopr[] = "kopr";
+
+        int i = 0;
+        while (kopr[i] != '\0') {
+            printf("%c\n", kopr[i]);
+            i++;
+        }
+
         
-
-
-
         return 0;
     }
+*/
+
+#include <stdio.h>
+#include <ctype.h>
+    int main () {
+
+        char ahoj[] = "AHOJ";
+        char znak;
+
+        printf("Zadejte libovolny znak:");
+        scanf("%c", &znak);
+        for (int i = 0; i < 4; i++) {
+
+            znak = toupper (ahoj[i]);
+
+            if (znak != ahoj[i]) {
+                continue;
+            }
+            
+            else if (znak == ahoj[i]) {
+            printf("Znak %c je ve stringu %s", znak, ahoj);
+            return 0;
+            }
+    }
+        printf("Znak %c neni ve stringu %s", znak, ahoj);
+
+        return 0;
+}
+
